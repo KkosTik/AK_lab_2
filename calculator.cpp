b08cbe3a66b77a36c989a560cfa421d9d152b6c7
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:45:44 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,16 @@
 #include "calculator.h"
-
+// start
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
-
+/// <summary>description</summary>
+/// <summary>MyMethod is a method in the MyClass class.
+/// <para>Here's how you could make a second paragraph in a description. <see cref="System::Console::WriteLine"/> for information about output statements.</para>
+/// <seealso cref="MyClass::MyMethod2"/>
+/// </summary>
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+// end
